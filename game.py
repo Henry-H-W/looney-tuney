9cb833644e38ec315e6c1a3bae787c5f5984bf7d
Richard Augustine <hirichard95@gmail.com>
2025-03-12 16:25:58 -0400

    editted firework visual

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """
 game.py - main in-game scene classes
 """
@@ -10,7 +8,6 @@ from pygame.locals import *
 from gamedirector import *
 
 import random
-import colorsys
 
 FW_FLYSPEED = 1.0 / 30.0
 FW_EXPLODESPEED = 0.5 / 30.0
@@ -22,37 +19,52 @@ class Firework(object):
     def __init__(self, parent, pos_x, colour, intensity):
         self.parent = parent
         self.pos_x = pos_x
-        self.pos_y = 1.0
+        self.pos_y = random.uniform(0.2, 0.8)
         self.colour = colour
         self.intensity = intensity
-        self.to_y = 0.8 * random.random() + 0.1
 
-        self.state = 'flying'
+        self.state = 'explode'  # Start immediately in explosion state
         self.radius = 0
+        self.explosion_progress = 0  # Track explosion animation progress
 
     def Update(self):
-        if self.state == 'flying':
-            self.pos_y -= FW_FLYSPEED
-            if self.pos_y < self.to_y:
-                self.state = 'explode'
-        elif self.state == 'explode':
-            self.radius += FW_EXPLODESPEED
-            if self.radius > 0.3:
+        if self.state == 'explode':
+            self.explosion_progress += 0.04  # Adjust for expansion speed
+            ease_out_factor = 1 - (1 - self.explosion_progress) ** 3  # Smooth expansion
+            self.radius = 0.3 * ease_out_factor  # Expands fast at first, slows down
+
+            if self.radius >= 0.3:
                 self.state = 'finished'
 
     def Draw(self, screen):
         if self.state == 'flying':
             blip_size = max(int(self.parent.window_size[0] * FW_BLIPSIZE), 1)
-            pygame.draw.circle(screen, self.colour, (int(self.parent.window_size[0] * self.pos_x),
-                                                     int(self.parent.window_size[1] * self.pos_y)), blip_size, 0)
+            pygame.draw.circle(screen, self.colour, 
+                            (int(self.parent.window_size[0] * self.pos_x), 
+                                int(self.parent.window_size[1] * self.pos_y)), blip_size, 0)
+
         elif self.state == 'explode':
             radius = max(1, int(self.parent.window_size[0] * self.radius))
             wave_size = max(int(self.parent.window_size[0] * FW_WAVESIZE), 1)
             if wave_size > radius:
                 wave_size = radius
-            pygame.draw.circle(screen, self.colour, (int(self.parent.window_size[0] * self.pos_x),
-                                                     int(self.parent.window_size[1] * self.pos_y)), radius, wave_size)
+            
+            # Create a new transparent surface for the fading effect
+            explosion_surface = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
 
+            # Gradually fade out the outline by reducing the alpha value
+            alpha = max(0, 255 - int((self.radius / 0.3) * 255))  # Fades from 255 to 0
+
+            # Define the fading outline color (RGBA with alpha)
+            faded_color = (self.colour[0], self.colour[1], self.colour[2], alpha)
+
+            # Draw the fading ring on the new surface
+            pygame.draw.circle(explosion_surface, faded_color, (radius, radius), radius, wave_size)
+
+            # Blit (draw) the explosion_surface onto the main screen with transparency
+            screen.blit(explosion_surface, 
+                        (int(self.parent.window_size[0] * self.pos_x) - radius, 
+                        int(self.parent.window_size[1] * self.pos_y) - radius))
 
 class MainGame(GameScene):
     def __init__(self, director, window_size):
@@ -121,7 +133,7 @@ class MainGame(GameScene):
 
         # look for creating new ones (based on keyboard)
         if self.key_press:
-            colour = tuple([int(255 * i) for i in colorsys.hsv_to_rgb(self.fire_colour_ind, 1.0, 1.0)])
+            colour = (255, 255, 255)
             self.fireworks.append(Firework(self, 0.8 * random.random() + 0.1, colour, 1))
             self.fire_colour_ind += 0.08
             if self.fire_colour_ind > 1.0:
@@ -133,10 +145,11 @@ class MainGame(GameScene):
                 data = self.midi.read(1)
                 if len(data) == 0:
                     break
+                # print("MIDI Data:", data)  # Debug: Print raw data
                 (type, note, vel, stuff) = data[0][0]
-                if vel > 0:
+                if type == 149 and vel > 0:
                     x_pos = (0.8 * (note - 36) / (96.0 - 36.0) + 0.1)
-                    colour = tuple([int(255 * i) for i in colorsys.hsv_to_rgb(self.fire_colour_ind, 1.0, 1.0)])
+                    colour = (255, 255, 255)
                     self.fireworks.append(Firework(self, x_pos, colour, 1))
                     self.fire_colour_ind += 0.08
                     if self.fire_colour_ind > 1.0:
